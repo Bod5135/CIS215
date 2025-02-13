9c52b70353d976549faceeed25085d8c6809b442
Connor Heath <ctheath09gmail.com>
2025-02-13 13:20:50 -0500

    feat: added fieldset with legend and an otpgroup for prop and jet

--- practice_CIS215/form.php
+++ practice_CIS215/form.php
@@ -16,17 +16,29 @@
                     <label for='fav_plane_input'>what is your favorite plane:</label>
                     <input type='text' name='fav_plane' id='fav_plane_input'>
 
-                    <label for='is_military_input'>is it military:</label>
-                    <input type='radio' name='is_military' id='is_military_input'> Yes
-                    <input type='radio' name='is_military' id='is_not_military_input'> no
+                    
+                    <fieldset>
+                        <legend>is it military:</legend>
+                        <input type='radio' name='is_military' id='is_military_input'> Yes
+                        <input type='radio' name='is_military' id='is_not_military_input'> no
+                    </fieldset>
                     
                     <label for='num_planes_input'> How many plane have you seen today:</label>
                     <select name='num_planes' id='num_planes_input'>
-                        <option value="0">none</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
+                        <optgroup label='propeller'>
+                            <option value="0">none</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </optgroup>
+                        <optgroup label='jet'>
+                            <option value="0">none</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </optgroup>
                     </select>
                 </form>
             </main>
