853d1e66a9e322df9e796412a925aa5a943224d5
Connor Heath <ctheath09gmail.com>
2025-02-13 17:33:31 -0500

    feat: put form in divs

--- practice_CIS215/form.php
+++ practice_CIS215/form.php
@@ -6,40 +6,54 @@
         </head>
         <body>
             <main>
-                <form action='' method='post' class='planes_form'>
-                    <label for='name_input'>Enter name here: </label>
-                    <input type='text' name='name' id='name_input'>
+                <form action='submit.php' method='post' class='planes_form'>
+                    <div>
+                        <label for='name_input'>Enter name here: </label>
+                        <input type='text' name='name' id='name_input'>
+                    </div>
 
-                    <label for='email_input'>Enter your email:</label>
-                    <input type='email' name='email' id= 'email_input'>
+                    <div>
+                        <label for='email_input'>Enter your email:</label>
+                        <input type='email' name='email' id= 'email_input'>
+                    </div>
 
-                    <label for='fav_plane_input'>what is your favorite plane:</label>
-                    <input type='text' name='fav_plane' id='fav_plane_input'>
+                    <div>
+                        <label for='fav_plane_input'>what is your favorite plane:</label>
+                        <textarea name='fav_plane' id='fav_plane_input'>Enter answer here</textarea>
+                    </div>
 
+                    <div>
+                        <fieldset>
+                            <legend>is it military:</legend>
+                            <input type='radio' name='is_military' id='is_military_input'> Yes
+                            <input type='radio' name='is_military' id='is_not_military_input'> no
+                        </fieldset>
+                    </div>
                     
-                    <fieldset>
-                        <legend>is it military:</legend>
-                        <input type='radio' name='is_military' id='is_military_input'> Yes
-                        <input type='radio' name='is_military' id='is_not_military_input'> no
-                    </fieldset>
+                    <div>
+                        <label for='num_planes_input'> How many plane have you seen today:</label>
+                        <select name='num_planes' id='num_planes_input'>
+                            <optgroup label='propeller'>
+                                <option value='0'>none</option>
+                                <option value='1'>1</option>
+                                <option value='2'>2</option>
+                                <option value='3'>3</option>
+                                <option value='4'>4</option>
+                            </optgroup>
+                            <optgroup label='jet'>
+                                <option value='0'>none</option>
+                                <option value='1'>1</option>
+                                <option value='2'>2</option>
+                                <option value='3'>3</option>
+                                <option value='4'>4</option>
+                            </optgroup>
+                        </select>
+                    </div>
                     
-                    <label for='num_planes_input'> How many plane have you seen today:</label>
-                    <select name='num_planes' id='num_planes_input'>
-                        <optgroup label='propeller'>
-                            <option value="0">none</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                        </optgroup>
-                        <optgroup label='jet'>
-                            <option value="0">none</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                        </optgroup>
-                    </select>
+                    <div>
+                        <button type='submit' name='submit_button' id='submit_button'>Submit</button>
+                    </div>
+                    
                 </form>
             </main>
         </body>
