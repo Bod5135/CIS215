28e74022d8ec651f29ed3a5d82c73fc5346b007d
Connor Heath <ctheath09gmail.com>
2025-02-12 20:14:33 -0500

    feat: added form

---
+++ practice_CIS215/form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+    <html lang='eng-US'>
+        <head>
+            <meta charset='utf-8'>
+            <meta name='viewport' content='width=device-width,initial-scale=1'/>
+        </head>
+        <body>
+            <main>
+                <form action='' method='post' class='planes_form'>
+                    Enter name here: <input type='text' name='name' id='name_input'>
+
+                    Enter your email: <input type='email' name='email' id= 'email_input'>
+
+                    what is your favorite plane: <input type='text' name='fav_plane' id='fav_plane_input'>
+
+                    is it military: 
+                    <input type='radio' name='is_military' id='is_military_input'> Yes
+                    <input type='radio' name='is_military' id='is_not_military_input'> no
+                </form>
+            </main>
+        </body>
+    </html>
