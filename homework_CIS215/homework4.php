355e1f2f31d51d1a05fd0aae61708c5dd25aac99
Connor Heath <ctheath09gmail.com>
2025-02-23 12:57:43 -0500

    feat: added more questions

--- homework_CIS215/homework4.php
+++ homework_CIS215/homework4.php
@@ -6,10 +6,11 @@
         </head>
         <body>
             <main>
+                <H1>What guardian are you</H1>
                 <form action='submit4.php' method='post' class='Destiny_quiz'>
                     <div>
                         <fieldset>
-                            <legend>what animal do you like the most:</legend>
+                            <legend>What animal do you like the most:</legend>
                             <input type='radio' name='fav_animal' id='lion' value='1'> Lion
                             <input type='radio' name='fav_animal' id='snake' value='2'> Snake
                             <input type='radio' name='fav_animal' id='hawk' value='3'> Hawk
@@ -23,18 +24,54 @@
                             <input type='radio' name='fav_role' id='support' value='3'> support
                         </fieldset>
                     </div>
+                    <div>
+                        <fieldset>
+                                <legend>How would you fight:</legend>
+                                <input type='radio' name='fav_fight' id='fists' value='1'> Fists
+                                <input type='radio' name='fav_fight' id='knife' value='2'> Knife
+                                <input type='radio' name='fav_fight' id='magic' value='3'> Magic
+                        </fieldset>
+                    </div>
+                    <div>
+                        <fieldset>
+                            <legend>What ablitiy do you think is the best:</legend>
+                            <input type='radio' name='fav_ablitiy' id='wall' value='1'> A wall
+                            <input type='radio' name='fav_ablitiy' id='dodge' value='2'> dodge
+                            <input type='radio' name='fav_ablitiy' id='heal'  value='3'> healing circle
+                        </fieldset>
+                    </div>
+                    <div>
+                        <fieldset>
+                            <legend>How do you like to spend time:</legend>
+                            <input type='radio' name='fav_passTime' id='sprots' value='1'> sports
+                            <input type='radio' name='fav_passTime' id='alone' value='2'> chill by yourself
+                            <input type='radio' name='fav_passTime' id='read' value='3'> read books
+                        </fieldset>
+                    </div>
+                    <div>
+                        <fieldset>
+                            <legend>What color is better:</legend>
+                            <input type='radio' name='fav_color' id='red' value='1'> Red
+                            <input type='radio' name='fav_color' id='blue' value='2'> Blue
+                            <input type='radio' name='fav_color' id='yellow' value='3'> Yellow
+                        </fieldset>
+                    </div>
+                    <div>
+                        <fieldset>
+                            <legend>What sound like more fun to have:</legend>
+                            <input type='radio' name='fav_movement' id='glide' value='1'> Glide
+                            <input type='radio' name='fav_movement' id='double_jump' value='2'> Double jump
+                            <input type='radio' name='fav_movement' id='fly' value='3'> fly for a short distance
+                        </fieldset>
+                    </div>
+                    <div>
                     <fieldset>
-                            <legend>how would you fight:</legend>
-                            <input type='radio' name='fav_fight' id='fists' value='1'> Fists
-                            <input type='radio' name='fav_fight' id='knife' value='2'> Knife
-                            <input type='radio' name='fav_fight' id='magic' value='3'> Magic
-                    </fieldset>
-                    <fieldset>
-                        <legend>what ablitiy do you think is the best:</legend>
-                        <input type='radio' name='fav_ablitiy' id='wall' value='1'> A wall
-                        <input type='radio' name='fav_ablitiy' id='dodge' value='2'> dodge
-                        <input type='radio' name='fav_ablitiy' id='heal'  value='3'> healing circle
-                    </fieldset>
+                            <legend>What would be your weapon of choice:</legend>
+                                <input type='radio' name='fav_weapon' id='shield' value='1'> Shield
+                                <input type='radio' name='fav_weapon' id='gun' value='2'>  Gun
+                                <input type='radio' name='fav_weapon' id='magic' value='3'> Magic
+                            </fieldset>
+                    </div>
                     <div>
                         <button type='submit' name='submit_button' id='submit_button'>Submit</button>
                     </div>
