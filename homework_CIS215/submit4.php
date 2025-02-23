5e7c51a6d4dc0cfffcc4d7e2f0347b1f20b42d06
Connor Heath <ctheath09gmail.com>
2025-02-23 13:01:11 -0500

    feat: added post for questions

--- homework_CIS215/submit4.php
+++ homework_CIS215/submit4.php
@@ -3,6 +3,10 @@
     $fav_role = $_POST["fav_role"];
     $fav_fight = $_POST["fav_fight"];
     $fav_ablitiy = $_POST["fav_ablitiy"];
+    $fav_passTime =$_POST["fav_passTime"];
+    $fav_color =$_POST["fav_color"];
+    $fav_movement =$_POST["fav_movement"];
+    $fav_weapon =$_POST["fav_weapon"];
 
     $total =  $fav_animal + $fav_role + $fav_fight + $fav_ablitiy;
     print($total);
