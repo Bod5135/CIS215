513539298b17787fafdb558c6d6257270d3c752a
Connor Heath <ctheath09gmail.com>
2025-02-22 22:40:09 -0500

    feat: gave the questions values

--- homework_CIS215/homework4.php
+++ homework_CIS215/homework4.php
@@ -10,35 +10,32 @@
                     <div>
                         <fieldset>
                             <legend>what animal do you like the most:</legend>
-                            <input type='radio' name='fav_animal' id='lion'> Lion
-                            <input type='radio' name='fav_animal' id='snake'> Snake
-                            <input type='radio' name='fav_animal' id='hawk'> Hawk
+                            <input type='radio' name='fav_animal' id='lion' value='1'> Lion
+                            <input type='radio' name='fav_animal' id='snake' value='2'> Snake
+                            <input type='radio' name='fav_animal' id='hawk' value='3'> Hawk
                         </fieldset>
                     </div>
                     <div>
                         <fieldset>
                             <legend>What role do you play the most:</legend>
-                            <input type='radio' name='fav_role' id='tank'> tank
-                            <input type='radio' name='fav_role' id='damage'> damage
-                            <input type='radio' name='fav_role' id='support'> support
+                            <input type='radio' name='fav_role' id='tank' value='1'> tank
+                            <input type='radio' name='fav_role' id='damage' value='2'> damage
+                            <input type='radio' name='fav_role' id='support' value='3'> support
                         </fieldset>
                     </div>
                     <fieldset>
                             <legend>how would you fight:</legend>
-                            <input type='radio' name='fav_fight' id='fists'> Fists
-                            <input type='radio' name='fav_fight' id='knife'> Knife
-                            <input type='radio' name='fav_fight' id='magic'> Magic
+                            <input type='radio' name='fav_fight' id='fists' value='1'> Fists
+                            <input type='radio' name='fav_fight' id='knife' value='2'> Knife
+                            <input type='radio' name='fav_fight' id='magic' value='3'> Magic
                     </fieldset>
                     <fieldset>
                         <legend>what ablitiy do you think is the best:</legend>
-                        <input type='radio' name='fav_ablitiy' id='wall'> A wall
-                        <input type='radio' name='fav_ablitiy' id='dodge'> dodge
-                        <input type='radio' name='fav_ablitiy' id='heal'> healing circle
+                        <input type='radio' name='fav_ablitiy' id='wall' value='1'> A wall
+                        <input type='radio' name='fav_ablitiy' id='dodge' value='2'> dodge
+                        <input type='radio' name='fav_ablitiy' id='heal' > healing circle
                     </fieldset>
                 </form>
-                <?php 
-
-                ?>
             </main>
         </body>
     </html>
