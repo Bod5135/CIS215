38c4a889628b9c60d83a60b6468d6ba8e18ff5dc
Connor Heath <ctheath09gmail.com>
2025-02-22 22:24:29 -0500

    feat: added questions

--- homework_CIS215/homework4.php
+++ homework_CIS215/homework4.php
@@ -6,8 +6,38 @@
         </head>
         <body>
             <main>
+                <form action='' method='post' class='Destiny_quiz'>
+                    <div>
+                        <fieldset>
+                            <legend>what animal do you like the most:</legend>
+                            <input type='radio' name='fav_animal' id='lion'> Lion
+                            <input type='radio' name='fav_animal' id='snake'> Snake
+                            <input type='radio' name='fav_animal' id='hawk'> Hawk
+                        </fieldset>
+                    </div>
+                    <div>
+                        <fieldset>
+                            <legend>What role do you play the most:</legend>
+                            <input type='radio' name='fav_role' id='tank'> tank
+                            <input type='radio' name='fav_role' id='damage'> damage
+                            <input type='radio' name='fav_role' id='support'> support
+                        </fieldset>
+                    </div>
+                    <fieldset>
+                            <legend>how would you fight:</legend>
+                            <input type='radio' name='fav_fight' id='fists'> Fists
+                            <input type='radio' name='fav_fight' id='knife'> Knife
+                            <input type='radio' name='fav_fight' id='magic'> Magic
+                    </fieldset>
+                    <fieldset>
+                        <legend>what ablitiy do you think is the best:</legend>
+                        <input type='radio' name='fav_ablitiy' id='wall'> A wall
+                        <input type='radio' name='fav_ablitiy' id='dodge'> dodge
+                        <input type='radio' name='fav_ablitiy' id='heal'> healing circle
+                    </fieldset>
+                </form>
                 <?php 
-                                                 
+
                 ?>
             </main>
         </body>
