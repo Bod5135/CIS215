4a81c99ade49d030e33e7f769885b6023b14dbc0
Connor Heath <ctheath09gmail.com>
2025-02-23 21:51:25 -0500

    feat: put every thing into functions and made a main function

--- homework_CIS215/submit4.php
+++ homework_CIS215/submit4.php
@@ -1,21 +1,37 @@
 <?php
+    main()
+    
+    function main(){
+        get_points()
+        get_total()
+        responed()
+    }
     /*this grabs all the values from the radio buttons */
-    $fav_animal = $_POST["fav_animal"];
-    $fav_role = $_POST["fav_role"];
-    $fav_fight = $_POST["fav_fight"];
-    $fav_ablitiy = $_POST["fav_ablitiy"];
-    $fav_passTime =$_POST["fav_passTime"];
-    $fav_color =$_POST["fav_color"];
-    $fav_movement =$_POST["fav_movement"];
-    $fav_weapon =$_POST["fav_weapon"];
+    function get_points(){
+        $fav_animal = $_POST["fav_animal"];
+        $fav_role = $_POST["fav_role"];
+        $fav_fight = $_POST["fav_fight"];
+        $fav_ablitiy = $_POST["fav_ablitiy"];
+        $fav_passTime =$_POST["fav_passTime"];
+        $fav_color =$_POST["fav_color"];
+        $fav_movement =$_POST["fav_movement"];
+        $fav_weapon =$_POST["fav_weapon"]; 
+    }
+    
     /*adds all the values together then puts the sum into total to help decide the responce*/
-    $total =  $fav_animal + $fav_role + $fav_fight + $fav_ablitiy + $fav_passTime + $fav_color + $fav_movement + $fav_weapon;
+    function get_total(){
+        $total =  $fav_animal + $fav_role + $fav_fight + $fav_ablitiy + $fav_passTime + $fav_color + $fav_movement + $fav_weapon;
+    }
+    
     /*decides what responce you get based on the number for total*/
-    if ($total <= 6) {
-        print("<h1>You are a titan</h1>");
-    }elseif ($total <= 16) {
-        print("<h1>You are a hunter</h1>");
-    }else {
-        print("<h1>You are a warlock</h1>");
-    };
+    function responed(){
+        if ($total <= 6) {
+                print("<h1>You are a titan</h1>");
+            }elseif ($total <= 16) {
+                print("<h1>You are a hunter</h1>");
+            }else {
+                print("<h1>You are a warlock</h1>");
+            };
+    }
+    
 ?>
