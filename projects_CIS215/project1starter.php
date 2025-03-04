fc24ed92fcb87eb5da9a9c002d0af5e587f1ccb9
Connor Heath <ctheath09gmail.com>
2025-03-03 19:13:20 -0500

    feat: move the radio buttons into a fieldset and made the first label into a legend

--- projects_CIS215/project1starter.php
+++ projects_CIS215/project1starter.php
@@ -14,34 +14,37 @@
 
         <label for="pw-id">Enter your password: </label>
         <input type="text" name="pw-name" id="pw-id">
-
-        <label for="">What age are you? </label>
-        <input type="radio" name="age" id="0-12">
-        <label for="0-12">0-12 </label>
-        <input type="radio" name="age" id="13-17">
-        <label for="13-17">13-17 </label>
-        <input type="radio" name="age" id="18-22">
-        <label for="18-22">18-22 </label>
-        <input type="radio" name="age" id="23-27">
-        <label for="23-27">23-27 </label>
-        <input type="radio" name="age" id="28-32">
-        <label for="28-32">28-32 </label>
-        <input type="radio" name="age" id="33-37">
-        <label for="33-37">33-37 </label>
-        <input type="radio" name="age" id="38-42">
-        <label for="38-42">38-42 </label>
-        <input type="radio" name="age" id="43-47">
-        <label for="43-47">43-47 </label>
-        <input type="radio" name="age" id="48-52">
-        <label for="48-52">48-52 </label>
-        <input type="radio" name="age" id="53-57">
-        <label for="53-57">53-57 </label>
-        <input type="radio" name="age" id="58-62">
-        <label for="58-62">58-62 </label>
-        <input type="radio" name="age" id="63-67">
-        <label for="63-67">63-67 </label>
-        <input type="radio" name="age" id="68+">
-        <label for="68+">68+ </label>
+        
+        <fieldset>
+            <legend>What age are you?</legend>
+            <input type="radio" name="age" id="0-12">
+            <label for="0-12">0-12 </label>
+            <input type="radio" name="age" id="13-17">
+            <label for="13-17">13-17 </label>
+            <input type="radio" name="age" id="18-22">
+            <label for="18-22">18-22 </label>
+            <input type="radio" name="age" id="23-27">
+            <label for="23-27">23-27 </label>
+            <input type="radio" name="age" id="28-32">
+            <label for="28-32">28-32 </label>
+            <input type="radio" name="age" id="33-37">
+            <label for="33-37">33-37 </label>
+            <input type="radio" name="age" id="38-42">
+            <label for="38-42">38-42 </label>
+            <input type="radio" name="age" id="43-47">
+            <label for="43-47">43-47 </label>
+            <input type="radio" name="age" id="48-52">
+            <label for="48-52">48-52 </label>
+            <input type="radio" name="age" id="53-57">
+            <label for="53-57">53-57 </label>
+            <input type="radio" name="age" id="58-62">
+            <label for="58-62">58-62 </label>
+            <input type="radio" name="age" id="63-67">
+            <label for="63-67">63-67 </label>
+            <input type="radio" name="age" id="68+">
+            <label for="68+">68+ </label>
+        </fieldset>
+        
 
         <select name="gender" id="gender">
             <option value="m">Male</option>
