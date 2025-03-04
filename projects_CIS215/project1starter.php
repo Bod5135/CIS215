40d644d0e2f5f953f6e90c565dafd5a259222233
Connor Heath <ctheath09gmail.com>
2025-03-03 19:08:15 -0500

    fix: gave lable the for property and fixed the ids and the name of the radio buttons

--- projects_CIS215/project1starter.php
+++ projects_CIS215/project1starter.php
@@ -9,39 +9,39 @@
     <!-- TODO: Fix all bugs/poor practice in the form -->
     <form action="" method="get" class="survey">
 
-        <label>Enter your email: </label>
+        <label for="email-id">Enter your email: </label>
         <input type="email" name="email-name" id="email-id">
 
-        <label>Enter your password: </label>
+        <label for="pw-id">Enter your password: </label>
         <input type="text" name="pw-name" id="pw-id">
 
-        <label>What age are you? </label>
-        <input type="radio" name="0" id="1">
-        <label>0-12 </label>
-        <input type="radio" name="1" id="1">
-        <label>13-17 </label>
-        <input type="radio" name="2" id="1">
-        <label>18-22 </label>
-        <input type="radio" name="3" id="1">
-        <label>23-27 </label>
-        <input type="radio" name="4" id="1">
-        <label>28-32 </label>
-        <input type="radio" name="5" id="1">
-        <label>33-37 </label>
-        <input type="radio" name="6" id="1">
-        <label>38-42 </label>
-        <input type="radio" name="7" id="1">
-        <label>43-47 </label>
-        <input type="radio" name="8" id="1">
-        <label>48-52 </label>
-        <input type="radio" name="9" id="1">
-        <label>53-57 </label>
-        <input type="radio" name="10" id="1">
-        <label>58-62 </label>
-        <input type="radio" name="11" id="1">
-        <label>63-67 </label>
-        <input type="radio" name="12" id="1">
-        <label>68+ </label>
+        <label for="">What age are you? </label>
+        <input type="radio" name="age" id="0-12">
+        <label for="0-12">0-12 </label>
+        <input type="radio" name="age" id="13-17">
+        <label for="13-17">13-17 </label>
+        <input type="radio" name="age" id="18-22">
+        <label for="18-22">18-22 </label>
+        <input type="radio" name="age" id="23-27">
+        <label for="23-27">23-27 </label>
+        <input type="radio" name="age" id="28-32">
+        <label for="28-32">28-32 </label>
+        <input type="radio" name="age" id="33-37">
+        <label for="33-37">33-37 </label>
+        <input type="radio" name="age" id="38-42">
+        <label for="38-42">38-42 </label>
+        <input type="radio" name="age" id="43-47">
+        <label for="43-47">43-47 </label>
+        <input type="radio" name="age" id="48-52">
+        <label for="48-52">48-52 </label>
+        <input type="radio" name="age" id="53-57">
+        <label for="53-57">53-57 </label>
+        <input type="radio" name="age" id="58-62">
+        <label for="58-62">58-62 </label>
+        <input type="radio" name="age" id="63-67">
+        <label for="63-67">63-67 </label>
+        <input type="radio" name="age" id="68+">
+        <label for="68+">68+ </label>
 
         <select name="gender" id="gender">
             <option value="m">Male</option>
