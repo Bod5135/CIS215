2be66b74b0e493b434e20710c81fb84e5b972243
Connor Heath <ctheath09gmail.com>
2025-03-04 20:17:33 -0500

    feat: added age validator

--- projects_CIS215/password_checker.php
+++ projects_CIS215/password_checker.php
@@ -5,6 +5,7 @@
         if (password_verify($_POST["pw-name"], "$2y$10$2PH3daYyS2gIKkDSJPa1NOgM4c26nDevLe5zZqJ3fpoIIBqxCQQke")) {
             print("<p>pasword vaild</p>");
             email_check();
+            age_check();
         } else {
             print("<p>password invaild</p>");
         };
@@ -12,9 +13,17 @@
     function email_check(){
         $email = $_POST["email-name"];
         if (str_contains($email, "@")) {
-           echo ("email valid");
+           echo (" email valid");
         }else {
-            echo ("email invalid");
+            echo (" email invalid");
+        };
+    }
+    function age_check(){
+        $age = $_POST["age"];
+        if ($age > 0) {
+           echo (" age valid");
+        }else {
+            echo (" age invalid");
         };
     }  
     ?>
