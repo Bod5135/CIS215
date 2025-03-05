9020b59e2b87ff22a5cd28dd5baf8ffbc16d46f2
Connor Heath <ctheath09gmail.com>
2025-03-04 20:03:04 -0500

    feat: added email validator

--- projects_CIS215/password_checker.php
+++ projects_CIS215/password_checker.php
@@ -4,8 +4,17 @@
     function main(){
         if (password_verify($_POST["pw-name"], "$2y$10$2PH3daYyS2gIKkDSJPa1NOgM4c26nDevLe5zZqJ3fpoIIBqxCQQke")) {
             print("<p>pasword vaild</p>");
+            email_check();
         } else {
             print("<p>password invaild</p>");
         };
-    };  
+    };
+    function email_check(){
+        $email = $_POST["email-name"];
+        if (str_contains($email, "@")) {
+           echo ("email valid");
+        }else {
+            echo ("email invalid");
+        };
+    }  
     ?>
