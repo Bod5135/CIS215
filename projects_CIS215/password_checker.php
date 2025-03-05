840e8ac37221e15e1ece9dcafb57ead616e3f3ac
Connor Heath <ctheath09gmail.com>
2025-03-04 19:46:58 -0500

    fix: changed the password cause i accidently commited it and put everything in functions

--- projects_CIS215/password_checker.php
+++ projects_CIS215/password_checker.php
@@ -1,9 +1,11 @@
 <?php
-        print(password_hash("willy", PASSWORD_DEFAULT));
+    main();
 
-        if (password_verify($_POST["pw-name"], "$2y$10\$N59ac8yDcFJ21t20hApZUuqfAY3X9NM106565fAfLkqJzG2DuehNC")) {
+    function main(){
+        if (password_verify($_POST["pw-name"], "$2y$10$2PH3daYyS2gIKkDSJPa1NOgM4c26nDevLe5zZqJ3fpoIIBqxCQQke")) {
             print("<p>pasword vaild</p>");
         } else {
             print("<p>password invaild</p>");
         };
+    };  
     ?>
