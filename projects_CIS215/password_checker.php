b709bea08636184fb435cd98c0a6ebff1dd727ba
Connor Heath <ctheath09gmail.com>
2025-03-10 19:04:47 -0400

    feat: made funtion to validate all the data from the form

--- projects_CIS215/password_checker.php
+++ projects_CIS215/password_checker.php
@@ -6,67 +6,61 @@
         $db = connectDB();
         if (password_verify($_POST["pw-name"], "$2y$10$2PH3daYyS2gIKkDSJPa1NOgM4c26nDevLe5zZqJ3fpoIIBqxCQQke")) {
             print("<p>pasword vaild</p>");
-            email_check();
-            age_check();
-            gender_checker();
-            pets_name_checker();
-            pets_age_checker();
+            data_checker();
         } else {
             print("<p>password invaild</p>");
         };
     };
     function data_checker(){
-        if (condition) {
-            
+        if (email_check()) {
+            if(age_check()){
+                if(gender_checker()){
+                    if (pets_name_checker()) {
+                        if (pets_age_checker()) {
+                           print("<p>all data valid</p>");
+                        };
+                    };
+                };
+            };
         };
     }
     function email_check(){
         $email = $_POST["email-name"];
         if (str_contains($email, "@")) {
-           print ("<p>email valid</p>");
            return true;
         }else {
-            print ("<p>email invalid</p>");
             return false;
         };
     }
     function age_check(){
         $age = $_POST["age"];
         if ($age > 0) {
-            print ("<p>age valid</p>");
             return true;
-        }else {
-            print ("<p>age invalid</p>");
+        }else {            
             return false;
         };
     }
     function gender_checker(){
         $gender = $_POST["gender"];
         if ($gender > 0) {
-            print ("<p>gender $gender</p>");
             return true;
          }else {
-            print ("<p>please selcet an answer</p>");
             return false;
          };
     }
     function pets_name_checker(){
         $pets_name = $_POST["pets_name"];
         if ($pets_name > 0) {
-            print ("<p>Your pets name is $pets_name</p>");
             return true;
          }else {
-            print ("<p>please answer pets name</p>");
             return false;
          };
     }
     function pets_age_checker(){
         $pets_age = $_POST["pets_age"];
         if ($pets_age > 0) {
-            print ("<p>Your pets age is $pets_age</p>");
             return true;
          }else {
-            print ("<p>please answer pets age</p>");
             return false;
          };
     }  
